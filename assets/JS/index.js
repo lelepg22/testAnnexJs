52fd2bc56b65e8a035a3758f1705a5f688eb231f
Alexandre Gaglianone <78416452+lelepg22@users.noreply.github.com>
2022-02-22 13:24:52 +0100

    Add files via upload

---
+++ assets/JS/index.js
@@ -0,0 +1,25 @@
+function submitCode() { 
+    localStorage.code = ""
+    
+    var value =  document.getElementById("codePostal").value;
+    
+    chars = document.getElementById("codePostal").value;
+
+    val = chars.charAt(0) + chars.charAt(1);
+    
+    if(value.length != 5) { alert('Il faut 5 chiffre'); return location.reload()}
+    if(!value.match("^[0-9]*$")){alert('Seulement des chiffres'); return location.reload()}
+
+    if(val == "31"){
+        localStorage.code = "Toulouse";
+        return location.assign("page2.html")
+    }
+
+    localStorage.code = "Bordeaux";
+    location.assign("page2.html")
+}
+
+window.onload = function() {
+    if(document.getElementById("resultat")){
+        document.getElementById("resultat").innerHTML = localStorage.code};
+  };
